bf49520c611107339c593f219a58751a69ac3627
SandyDRawat <sr9644774485@gmail.com>
2025-02-24 18:16:56 +0530

    Modify Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM python:3.10
 
 WORKDIR /app
-COPY requirements1.txt .
+COPY requirements.txt .
 # Install dependencies
 RUN pip install --upgrade pip
-RUN pip install -r requirements1.txt
+RUN pip install -r requirements.txt
 
 COPY . /app
 
 # Run the serverless handler
-CMD ["python3","u", "/app/server_eg.py"]
+CMD ["python3","-u", "/app/server.py"]
--- call.py
+++ call.py
@@ -8,7 +8,7 @@ dotenv.load_dotenv()
 
 RP_API_KEY =  os.getenv("RP_API_KEY") 
 
-def send_request(image_path, question, url="https://api.runpod.ai/v2/9jsqfbvo0yhvdg/runsync"): # change the url to your server url
+def send_request(image_path, question, url="https://api.runpod.ai/v2/l32k9po3kztez6/runsync"): # change the url to your server url
     # Convert image to base64
     with open(image_path, "rb") as img_file:
         base64_image = base64.b64encode(img_file.read()).decode("utf-8")
@@ -19,7 +19,6 @@ def send_request(image_path, question, url="https://api.runpod.ai/v2/9jsqfbvo0yh
         "input": {
             "image": base64_image,
             "question": question,
-            "seconds": 0
         }
     }
 
@@ -29,7 +28,7 @@ def send_request(image_path, question, url="https://api.runpod.ai/v2/9jsqfbvo0yh
         'Authorization': RP_API_KEY
     }
     response = requests.post(url, json=data, headers=headers)
-
+    print(response)
     # Print response
     print("Status Code:", response.status_code)
     try:
