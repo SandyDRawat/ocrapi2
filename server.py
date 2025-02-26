492eee4150ace8e75387caa877010d2d13bd4598
SandyDRawat <sr9644774485@gmail.com>
2025-02-26 21:31:43 +0530

    Modify server

--- server.py
+++ server.py
@@ -7,32 +7,32 @@ import os
 from base64 import b64decode
 import time
 
+MODEL_PATH = "/app/model/MiniCPM-V-2_6-int4"
+    # Check if GPU is available
+device = "cuda"
+
+# Load model, tokenizer, and processor from local path
+def load_local_model(model_path: str, device: str):
+    model = AutoModel.from_pretrained(model_path,trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(model_path,trust_remote_code=True)
+    return model, tokenizer
+
+# Load model at startup
+try:
+    model, tokenizer = load_local_model(MODEL_PATH, device)
+    model.to(device)
+    model.eval()
+except Exception as e:
+    print(f"Error loading model: {e}")
+    exit(1)
+
 def handler(event):
     input = event['input']
     question = input.get('question')
     image64 = input.get('image')
     image = Image.open(BytesIO(b64decode(image64)))
     #seconds = input.get('seconds', 0)
-
-    MODEL_PATH = "/app/model/MiniCPM-V-2_6-int4"
-    # Check if GPU is available
-    device = "cuda"
-
-    # Load model, tokenizer, and processor from local path
-    def load_local_model(model_path: str, device: str):
-        model = AutoModel.from_pretrained(model_path,trust_remote_code=True)
-        tokenizer = AutoTokenizer.from_pretrained(model_path,trust_remote_code=True)
-        processor = AutoProcessor.from_pretrained(model_path,trust_remote_code=True)
-        return model, tokenizer, processor
-
-    # Load model at startup
-    try:
-        model, tokenizer, processor = load_local_model(MODEL_PATH, device)
-        model.to(device)
-        model.eval()
-    except Exception as e:
-        print(f"Error loading model: {e}")
-        exit(1)
+   
 
     # Prepare input
     msgs = [{"role": "user", "content": [image, question]}]
